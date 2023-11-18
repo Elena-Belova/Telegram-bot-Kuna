4c1619b5d38b57392e1e390f14a1e42d48778110
Elena-Belova <volobelo@mail.ru>
2023-11-18 14:35:27 +0300

    Add files

---
+++ KunaBot.py
@@ -0,0 +1,38 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, Converter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = "Привет! Я бот Куна. Для конвертации валют отправьте мне сообщение в следующем формате:\n \
+<имя валюты, цену которой Вы хотите узнать> пробел \n <имя валюты, в которую надо перевести> пробел \n <количество переводимой валюты>\n \
+Пример сообщения: доллар евро 1\n Увидеть список доступных валют: /values"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты: "
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise APIException("Слишком много/мало параметров.")
+        base, quote, amount = values
+        total_base = Converter.get_price(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя.\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду.\n{e}")
+    else:
+        text = f"Стоимость {amount} {base} в {quote} = {total_base}"
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
+
